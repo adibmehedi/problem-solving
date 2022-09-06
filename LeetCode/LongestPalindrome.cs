aca80cbfa0ef8c15fc6ebfb392df723eb6bd5913
adibmehedi <adibmehedi@gmail.com>
2022-09-06 23:52:41 +0600

    Longest Palindrome & N aray tree preorder traversal solved

---
+++ LeetCode/LongestPalindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class LongestPalindrome
+    {
+        public int LongestPalindromeSolution(string s)
+        {
+            char[] arr = s.ToCharArray();
+            HashSet<char> collection = new HashSet<char>();
+            int doubleCounter = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (collection.Contains(arr[i]))
+                {
+                    doubleCounter++;
+                    collection.Remove(arr[i]);
+                }
+                else {
+                    collection.Add(arr[i]);
+                }
+            }
+
+            return collection.Count > 0 ? doubleCounter * 2 + 1 : doubleCounter * 2;
+
+        }
+    }
+}
---
+++ LeetCode/NAryTreePreorderTraversal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class NAryTreePreorderTraversal
+    {
+        public IList<int> Travarse(Node root, IList<int> list)
+        {
+            if (root == null) { return list; }
+
+            list.Add(root.val);
+            int childCount = root.children.Count;
+            if (childCount > 0)
+            {
+                int counter = 0;
+                while (counter < childCount)
+                { 
+                    this.Travarse(root.children[counter], list);
+                    counter++;
+                }
+            }
+            return list;
+        }
+        public IList<int> Preorder(Node root)
+        {
+            return this.Travarse(root, new List<int>());
+        }
+    }
+}
