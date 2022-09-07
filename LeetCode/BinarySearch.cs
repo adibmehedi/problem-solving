3e57f724f7ebf4fd58fdb7365acbd16f61c3a227
adibmehedi <adibmehedi@gmail.com>
2022-09-07 23:30:38 +0600

    Tree level order & Binary serach solved

---
+++ LeetCode/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+
+            int left = 0;
+            int right = nums.Length -1;
+
+            while (left <= right)
+            {
+                int pointer = left + (right - left) / 2;
+                if (nums[pointer] == target) { return pointer; }
+
+                if (nums[pointer] > target) // go left
+                {
+                    right = pointer -1;
+                }
+
+                if (nums[pointer] < target) // go right
+                {
+                    left = pointer + 1;
+                }
+            }
+
+            return -1;
+
+        }
+    }
+}
---
+++ LeetCode/BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class BinaryTreeLevelOrderTraversal
+    {
+
+        public IList<IList<int>> Travarse(TreeNode root, IList<IList<int>> list, int level)
+        {
+            if (root == null) return list;
+            //Console.WriteLine($"{level} {root.val} {list.Count}");
+
+            if (list.Count == level) list.Add(new List<int>());
+            list[level].Add(root.val);
+
+            this.Travarse(root.left, list, level + 1);
+            this.Travarse(root.right, list, level + 1);
+
+            return list;
+        }
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            return this.Travarse(root, list, 0);
+        }
+    }
+}
