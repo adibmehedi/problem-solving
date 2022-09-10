7cc64ac3bd117d0e5eadc31741be4cc72bc1dd55
adibmehedi <adibmehedi@gmail.com>
2022-09-10 16:05:34 +0600

    Validate binary serach tree solved

---
+++ LeetCode/ValidateBinarySearchTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class ValidateBinarySearchTree
+    {
+        public bool Validate(TreeNode node, long min, long max)
+        {
+            if (node == null) { return true; }
+
+            if (node.val >= max || node.val <= min) { return false; }
+
+            return Validate(node.left, min, node.val) && Validate(node.right, node.val, max); ;
+        }
+        public bool IsValidBST(TreeNode root)
+        {
+            return Validate(root, long.MinValue, long.MaxValue);
+        }
+    }
+}
