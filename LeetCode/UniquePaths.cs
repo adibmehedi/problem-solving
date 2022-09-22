3a009486f243ec6caf5a0dce8b94c05663b42f90
adibmehedi <adibmehedi@gmail.com>
2022-09-22 09:46:34 +0600

    Unique path solved

---
+++ LeetCode/UniquePaths.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class UniquePathsSolution
+    {
+        int CalculateCost(int m, int n, int row, int column, int[,]cache)
+        {
+            if (row == m - 1 && column == n - 1) { return 1; }
+            if (row >= m || column >= n) { return 0; }
+
+            if (cache[row, column] == -1)
+            {
+                cache[row, column] = CalculateCost(m, n, row + 1, column, cache) + CalculateCost(m, n, row, column + 1, cache);
+            }
+            return cache[row, column];
+        }
+
+        public int UniquePaths(int m, int n)
+        {
+            int[,] cache = new int[m, n];
+            for (int i = 0; i < m; i++) 
+            {
+                for (int j = 0; j < n; j++) 
+                {
+                    cache[i,j] = -1;
+                }
+            }
+            return CalculateCost(m, n, 0, 0, cache);
+        }
+    }
+}
