c79ab7b48d5a61f824fafc89ca96b2c6b8c49315
adibmehedi <adibmehedi@gmail.com>
2022-09-27 23:43:27 +0600

    Longest Repeating character replacement

---
+++ LeetCode/LongestRepeatingCharacterReplacement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    class LongestRepeatingCharacterReplacement
+    {
+        public int CharacterReplacement(string s, int k)
+        {
+            char[] sChars = s.ToCharArray();
+            int[] dictionaryCount = new int[26]; // dictinoary is slower
+            int leftPointer = 0;
+            int rightPointer = 0;
+            int result = 0;
+
+            while (rightPointer <= sChars.Length) 
+            {
+
+                int diffCharacters = dictionaryCount.Sum() - dictionaryCount.Max();
+                if (diffCharacters <= k) 
+                {
+                    result = Math.Max(result, rightPointer - leftPointer);
+                    if (rightPointer < sChars.Length) { dictionaryCount[sChars[rightPointer] - 'A']++; }
+                    rightPointer++;
+                }
+                else {
+                    dictionaryCount[sChars[leftPointer] - 'A']--;
+                    leftPointer++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
