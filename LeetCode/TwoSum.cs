0b7a2961ae8fd1e6d99c7d1e6ecdd4fb6fe8288a
adibmehedi <adibmehedi@gmail.com>
2022-09-28 10:34:10 +0600

    Two sum solved

---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int required = target - nums[i];
+                if (dic.ContainsKey(required))
+                {
+                    var requiredIndex = dic[required];
+                    return new int[] { i, requiredIndex };
+                }
+
+                if (!dic.ContainsKey(nums[i])) { dic.Add(nums[i], i); }
+            }
+
+            return result;
+        }
+    }
+}
