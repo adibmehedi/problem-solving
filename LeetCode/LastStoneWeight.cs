1c4b48be52c6d46623953ff3308aa2d0401c6781
adibmehedi <adibmehedi@gmail.com>
2022-10-12 02:55:18 +0600

    last stone weight sovled

---
+++ LeetCode/LastStoneWeight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LastStoneWeightSolution
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+
+            var queue = new PriorityQueue<int, int>();
+
+            foreach (int stone in stones)
+            {
+                queue.Enqueue(stone, -stone);
+            }
+
+            while (queue.Count > 1)
+            {
+                int stone1 = queue.Dequeue();
+                int stone2 = queue.Dequeue();
+                if (stone1 != stone2) 
+                {
+                    int diff = Math.Abs(stone1 - stone2);
+                    queue.Enqueue(diff, -diff);
+                }
+            }
+
+            return queue.Count > 0 ? queue.Dequeue() : 0;
+
+        }
+    }
+}
