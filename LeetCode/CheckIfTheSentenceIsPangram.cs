30784e63994f30bc7b3f47a27f50e77edfb117ae
adibmehedi <adibmehedi@gmail.com>
2022-10-17 21:12:41 +0600

    Check if the sentense is panagram

---
+++ LeetCode/CheckIfTheSentenceIsPangram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class CheckIfTheSentenceIsPangram
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            List<char> charList = new List<char>();
+            foreach (char character in sentence)
+            {
+                if (!charList.Contains(character))
+                { 
+                    charList.Add(character);
+                }
+            }
+
+            return charList.Count == 27 ? true : false; 
+        }
+    }
+}
