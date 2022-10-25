59044ff68b80efcf3ea62f7fe08b288a8a5752f8
adibmehedi <adibmehedi@gmail.com>
2022-10-26 02:01:12 +0600

    Check if two string arrays equivalent sovled

---
+++ LeetCode/CheckIfTwoStringArraysAreEquivalent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class CheckIfTwoStringArraysAreEquivalent
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            return string.Join(string.Empty, word1) == string.Join(string.Empty, word2);
+        }
+    }
+}
